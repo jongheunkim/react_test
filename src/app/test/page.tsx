9a394c977ad7015313b2941e0f5b6571d0f8684d
rlawhdgms11 <rlawhdgms10@naver.com>
2023-08-27 16:24:30 +0900

    환경설정

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+
+function Test(): ReactElement {
+    return (
+        <>
+            <div>
+                aaa
+            </div>
+        </>
+    );
+}
+
+export default Test;
